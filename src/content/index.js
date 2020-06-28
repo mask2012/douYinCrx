a85ac534e870d0699d9af534fe3419a40e55c9d6
mask2012 <156544634@qq.com>
2020-06-28 16:39:24 +0800

    流程打通大半

--- src/content/index.js
+++ src/content/index.js
@@ -1,15 +1,89 @@
 console.log("============d1");
-// console.log(document.cookie);
 
+import $ from "jquery";
+
+import {
+  setStorage, //存值
+  getStorage, //取值
+} from "@/lib";
 
 var page = {
+  domInserted: false, //dom是否已插入
+  convertTitle:'',    //待转换的文章标题
+  convertUrl:'',      //待转换的文章url
 
-  init: function() {
-    // this.monitorPopup();
-    // this.bindEvent();
+  init: function () {
+    this.monitorPopup();
+    this.initMonitor();
+  },
+  //监听popup发来的消息：图片地址，询问搜狐id
+  monitorPopup: function () {
+    window.chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
+      console.log("request", request);
+      if (request.action == "isConvertable") {
+        const convertObj = this.getConvertObj();
+        console.log("convertObj", convertObj);
+        sendResponse(JSON.stringify(convertObj));
+      }
+    });
+  },
+  getConvertObj() {
+    return {
+      isConvertable: location.host == "mp.weixin.qq.com",
+      convertTitle: $("#activity-name").html().trim(),
+      convertUrl: location.href,
+    };
   },
 
-  
+  initMonitor() {
+    //发现来到抖音
+    if (location.host == "creator.douyin.com") {
+      getStorage({ convertTitle:'',convertUrl:'' }, (data) => {
+        this.convertTitle=data.convertTitle
+        this.convertUrl=data.convertUrl
+      });
+      setInterval(() => {
+        this.monitLogin(() => {
+          if (!this.domInserted) {
+            this.domInserted = true;
+            this.insertHintDom();
+            this.hintDomBindEvent();
+          }
+        });
+      }, 1500);
+    }
+  },
+  monitLogin(callback) {
+    console.log($(".semi-avatar").length > 0);
+    if ($(".semi-avatar").length > 0) {
+      callback();
+    }
+  },
+  insertHintDom: function () {
+    const html =
+      '<div id="dataMarketModal" class="data_market_modal">' +
+      '      <div class="data_market_modal_title">检测到您已登录<br>是否将《'+this.convertTitle+'》转为视频发布？</div>' +
+      '      <span id="dataMarketModalYes">Yes</span><span id="dataMarketModalNo">No</span>' +
+      "    </div>";
+    const css =
+      '<style id="dataMarketCss">.data_market_modal{ position: fixed; left: 0; top: 0; width: 100%; bottom: 0; z-index: 99999; display: flex; flex-direction: column; justify-content: center; align-items: center; background: rgba(0,0,0,.3);} .data_market_modal .data_market_modal_title{ font-size: 28px; text-align:center; margin-bottom: 30px;} .data_market_modal span{ display: inline-block; padding: 10px 33px; font-size: 24px; background: #000; color: #fff; border-radius: 5px; margin-bottom: 15px; cursor: pointer;} .data_market_modal span:hover{ background: #333;} @supports (backdrop-filter: blur(6px)) or (-webkit-backdrop-filter: blur(6px)) { .data_market_modal{ background: rgba(238,240,247,0.3); backdrop-filter:blur(6px); } }</style>';
+    $("body").append(html);
+    $("body").append(css);
+  },
+  hintDomBindEvent: function () {
+    $("#dataMarketModalYes").click(()=> {
+      console.log("yes clicked");
+      this.hideModal();
+    });
+    $("#dataMarketModalNo").click(() => {
+      this.hideModal();
+    });
+  },
+  hideModal: function () {
+    $("#dataMarketModal").css({
+      display: "none",
+    });
+  },
 };
 
 page.init();
--- src/popup/App/App.vue
+++ src/popup/App/App.vue
@@ -1,6 +1,103 @@
 <template>
   <div id="app">
-    hello everyone
+    <ul class="tast_list">
+      <li class="li_title">
+        <div class="t1">文章</div>
+        <div class="t2">状态</div>
+      </li>
+      <li>
+        <div class="t1">下半年我国旅游经济如何走？最新判断来了</div>
+        <div class="t2">进行中</div>
+      </li>
+      <li>
+        <div class="t1">
+          下半年我国旅游经济如何走？最新判断来了下半年我国旅游经济如何走？最新判断来了
+        </div>
+        <div class="t2">进行中</div>
+      </li>
+      <li>
+        <div class="t1">下半年我国旅游经济如何走？最新判断来了</div>
+        <div class="t2">进行中</div>
+      </li>
+      <li>
+        <div class="t1">
+          下半年我国旅游经济如何走？最新判断来了下半年我国旅游经济如何走？最新判断来了
+        </div>
+        <div class="t2">进行中</div>
+      </li>
+      <li>
+        <div class="t1">下半年我国旅游经济如何走？最新判断来了</div>
+        <div class="t2">进行中</div>
+      </li>
+      <li>
+        <div class="t1">
+          下半年我国旅游经济如何走？最新判断来了下半年我国旅游经济如何走？最新判断来了
+        </div>
+        <div class="t2">进行中</div>
+      </li>
+      <li>
+        <div class="t1">下半年我国旅游经济如何走？最新判断来了</div>
+        <div class="t2">进行中</div>
+      </li>
+      <li>
+        <div class="t1">
+          下半年我国旅游经济如何走？最新判断来了下半年我国旅游经济如何走？最新判断来了
+        </div>
+        <div class="t2">进行中</div>
+      </li>
+      <li>
+        <div class="t1">下半年我国旅游经济如何走？最新判断来了</div>
+        <div class="t2">进行中</div>
+      </li>
+      <li>
+        <div class="t1">
+          下半年我国旅游经济如何走？最新判断来了下半年我国旅游经济如何走？最新判断来了
+        </div>
+        <div class="t2">进行中</div>
+      </li>
+      <li>
+        <div class="t1">下半年我国旅游经济如何走？最新判断来了</div>
+        <div class="t2">进行中</div>
+      </li>
+      <li>
+        <div class="t1">
+          下半年我国旅游经济如何走？最新判断来了下半年我国旅游经济如何走？最新判断来了
+        </div>
+        <div class="t2">进行中</div>
+      </li>
+      <li>
+        <div class="t1">下半年我国旅游经济如何走？最新判断来了</div>
+        <div class="t2">进行中</div>
+      </li>
+      <li>
+        <div class="t1">
+          下半年我国旅游经济如何走？最新判断来了下半年我国旅游经济如何走？最新判断来了
+        </div>
+        <div class="t2">进行中</div>
+      </li>
+      <li>
+        <div class="t1">下半年我国旅游经济如何走？最新判断来了</div>
+        <div class="t2">进行中</div>
+      </li>
+      <li>
+        <div class="t1">
+          下半年我国旅游经济如何走？最新判断来了下半年我国旅游经济如何走？最新判断来了
+        </div>
+        <div class="t2">进行中</div>
+      </li>
+      <li>
+        <div class="t1">下半年我国旅游经济如何走？最新判断来了</div>
+        <div class="t2">进行中</div>
+      </li>
+      <li>
+        <div class="t1">
+          下半年我国旅游经济如何走？最新判断来了下半年我国旅游经济如何走？最新判断来了
+        </div>
+        <div class="t2">进行中</div>
+      </li>
+    </ul>
+
+    <div class="btn_start_job" v-if="isConvertable" @click="saveJumpDouYin"><span>将当前文章转视频发抖音</span></div>
   </div>
 </template>
 
@@ -12,7 +109,9 @@ import {
   setStorage, //存值
   getStorage, //取值
   debounce, //防抖
+  sendMsg2ActiveTab, //和content通讯
 } from "@/lib";
+import Fingerprint2 from "fingerprintjs2";
 
 export default {
   name: "App",
@@ -21,47 +120,74 @@ export default {
   },
   data() {
     return {
-      historySearchArr: [], //历史搜索数据
+      loading: null, //全局loading
+      isConnectedToContent: false, //默认未关联到content
+      isConvertable: false, //默认是不可转换的文章
+      convertTitle:'',  //转换文章的标题
+      convertUrl:'',    //转换文章的链接
     };
   },
 
   methods: {
-    getClientId() {
-      var client = new ClientJS();
-      var fingerprint = client.getFingerprint();
-      console.log("fingerprint", fingerprint);
+    getFingerprint() {
+      requestIdleCallback(function () {
+        var options = { userDefinedFonts: ["Nimbus Mono", "Junicode", "Presto"] };
+        Fingerprint2.get(options, function (components) {
+          var values = components.map(function (component) {
+            return component.value;
+          });
+          var murmur = Fingerprint2.x64hash128(values.join(""), 31);
+          console.log("murmur", murmur);
+        });
+      });
     },
-
-    // addToHistory(data) {
-    //   this.historySearchArr = this.historySearchArr.filter((item) => {
-    //     return item != data.name;
-    //   });
-    //   if (this.historySearchArr.length > 10) {
-    //     this.historySearchArr.pop();
-    //   }
-    //   this.historySearchArr.unshift(data.name);
-    //   setStorage({ historySearchArr: this.historySearchArr });
-    // },
-    // getHistorySearch() {
-    //   getStorage({ historySearchArr: [] }, (data) => {
-    //     this.historySearchArr = data.historySearchArr;
-    //     // console.log('this.historySearchArr',this.historySearchArr);
-    //     if (this.historySearchArr.length > 0) {
-    //       this.item.name = this.historySearchArr[0];
-    //       this.goResultByKeyword(this.historySearchArr[0]);
-    //     }
-    //   });
-    // },
+    checkIsConvertable() {
+      sendMsg2ActiveTab(
+        {
+          action: "isConvertable",
+        },
+        (response) => {
+          if (response) {
+            this.isConnectedToContent = true;
+            const obj=JSON.parse(response)
+            this.isConvertable = obj.isConvertable;
+            this.convertTitle = obj.convertTitle;
+            this.convertUrl = obj.convertUrl;
+          }
+        }
+      );
+    },
+    async saveJumpDouYin(){
+      setStorage({ 
+        convertTitle:this.convertTitle,
+        convertUrl:this.convertUrl,
+       });
+       this.loading = this.$loading({
+        fullscreen: true,
+        text: "即将跳转抖音，登录后即可自动发布",
+      });
+      await sleep(2000)
+      this.loading.close();
+      window.open("https://creator.douyin.com/");
+    }
   },
 
   mounted() {
     //设置axios超时时间
-    // axios.defaults.timeout = 10000;
+    axios.defaults.timeout = 10000;
 
-    //获取搜索历史
-    // this.getTaskList();
+    //获取当前用户唯一指纹id
+    this.getFingerprint();
 
-    this.getClientId();
+    //检查当前页面是否可转成视频
+    this.checkIsConvertable();
+
+    //确认可以与content保持通讯
+    setTimeout(() => {
+      if (!this.isConnectedToContent) {
+        this.$message.error("无法和页面通讯，请刷新页面后重试");
+      }
+    }, 2000);
   },
 };
 </script>
